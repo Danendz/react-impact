8857752a654050a74e3741763ed635694a3e5754
unknown <C:\Users\Dev\AppData\Roaming\The Bat!>
2022-06-25 15:05:07 +0100

    added text shadow and added box shadow to character info

--- src/Components/CharacterInfo/CharacterInfo.jsx
+++ src/Components/CharacterInfo/CharacterInfo.jsx
@@ -1,14 +1,54 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import cl from "./CharacterInfo.module.css";
 import Loader from "../UI/Loader/Loader";
 import Constelattions from "./Constellations/Constellations";
 
 const CharacterInfo = ({ isLoading, characterData }) => {
-  const [bgColor, setBgColor] = useState("");
+  const [bgColor, setBgColor] = useState({
+    contentColor: "black",
+    nameColor: "black",
+    boxShadowColor: "black",
+    buttonsBgColor: "black",
+  });
   const [vision, setVision] = useState("");
-  const visionImage = useRef();
+
+  const getColorWithAlpha = (color, alpha) => {
+    return `rgba(${color}, ${alpha})`;
+  };
+
   useEffect(() => {
-    const getNameBgColor = () => {
+    function getVisionBgColor() {
+      const colors = {
+        Cryo: "52, 152, 219",
+        Geo: "243, 157, 18",
+        Anemo: "46, 204, 112",
+        Electro: "156, 89, 182",
+        Pyro: "231, 77, 60",
+        Hydro: "41, 127, 185",
+      };
+      const currentVisionColor = colors[characterData["vision"]];
+
+      const nameBgColor = getColorWithAlpha(currentVisionColor, 0.6);
+      const contentBgColor = getColorWithAlpha(currentVisionColor, 0.1);
+      const boxShadowBgColor = getColorWithAlpha(currentVisionColor, 0.2);
+      const buttonsBgColor = getColorWithAlpha(currentVisionColor, 0.7);
+
+      setBgColor(
+        characterData["vision"]
+          ? {
+              contentColor: contentBgColor,
+              nameColor: nameBgColor,
+              boxShadowColor: boxShadowBgColor,
+              buttonsBgColor: buttonsBgColor,
+            }
+          : {}
+      );
+    }
+    getVisionBgColor();
+  }, [characterData]);
+
+  useEffect(() => {
+    const getVisionImgs = () => {
       const visions = {
         Cryo: "https://muakasan.github.io/genshin-portraits/assets/cryo.png",
         Geo: "https://muakasan.github.io/genshin-portraits/assets/geo.png",
@@ -18,23 +58,12 @@ const CharacterInfo = ({ isLoading, characterData }) => {
         Pyro: "https://muakasan.github.io/genshin-portraits/assets/pyro.png",
         Hydro: "https://muakasan.github.io/genshin-portraits/assets/hydro.png",
       };
-      const colors = {
-        Cryo: "rgba(52, 152, 219, 0.6)",
-        Geo: "rgba(243, 157, 18, 0.6)",
-        Anemo: "rgba(46, 204, 112, 0.6)",
-        Electro: "rgba(156, 89, 182, 0.6)",
-        Pyro: "rgba(231, 77, 60, 0.6)",
-        Hydro: "rgba(41, 127, 185, 0.6)",
-      };
-      setBgColor(
-        characterData["vision"] ? colors[characterData["vision"]] : "black"
-      );
       setVision(
         characterData["vision"] ? visions[characterData["vision"]] : "#"
       );
     };
-    getNameBgColor();
-  }, [characterData, vision]);
+    getVisionImgs();
+  }, [characterData]);
 
   if (isLoading) {
     return (
@@ -49,20 +78,21 @@ const CharacterInfo = ({ isLoading, characterData }) => {
       style={{ backgroundImage: `url(${characterData["card"]})` }}
       className={cl.info}
     >
-      <div className={cl.content}>
-        <p style={{ backgroundColor: bgColor }} className={cl.name}>
-          <img
-            ref={visionImage}
-            className={cl.img}
-            src={vision}
-            alt="vision pic"
-          />
+      <div
+        style={{
+          backgroundColor: bgColor.contentColor,
+          boxShadow: "0px 0px 59px 53px " + bgColor.boxShadowColor + " inset",
+        }}
+        className={cl.content}
+      >
+        <p style={{ backgroundColor: bgColor.nameColor }} className={cl.name}>
+          <img className={cl.img} src={vision} alt="vision pic" />
           {characterData["name"]}
         </p>
 
         <div className={cl.const}>
           <Constelattions
-            bgColor={bgColor}
+            bgColor={bgColor.buttonsBgColor}
             consts={characterData["constellations"]}
           />
         </div>
